fce9f554795e12995f450b01b76f839d65fdd009
vanshita00 <129608607+vanshita00@users.noreply.github.com>
2024-04-29 13:20:49 +0530

    Add files via upload

---
+++ banking-app/src/main/java/net/javaguides/bankingapp/service/AccountService.java
@@ -0,0 +1,16 @@
+package net.javaguides.bankingapp.service;
+import net.javaguides.bankingapp.dto.AccountDto;
+
+import java.util.List;
+
+
+public interface AccountService {
+    AccountDto createAccount(AccountDto accountDto);
+    AccountDto getAccountById(Long id);
+    AccountDto deposit(Long id, double amount);
+    AccountDto withdraw(Long id, double amount);
+    List<AccountDto> getAllAccounts();
+
+    void deleteAccount(Long id);
+
+}
